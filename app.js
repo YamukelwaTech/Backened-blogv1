7ce0f02054b30beb7cbc76825ba267f28e9261eb
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-05-21 06:41:34 +0200

    Chore:NewInfo

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+// Serve static files (images) from the "public" directory
+app.use('/assets', express.static(path.join(__dirname, './assets')));
 
 // Import routes
 const postsRoutes = require("./routes/routes");
