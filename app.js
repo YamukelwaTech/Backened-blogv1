1b744644136d0b5c8aea6a990f933915349515df
YamuTech <157011521+YamukelwaTech@users.noreply.github.com>
2024-05-24 09:46:26 +0200

    Chore:comment

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const http = require("http");
-const { router: postsRoutes, wss } = require("./routes/routes");
+// Import required modules
+const express = require("express"); // Importing Express.js framework
+const cors = require("cors"); // Importing CORS middleware
+// const path = require("path"); 
+const http = require("http"); // Importing Node.js HTTP module for creating HTTP server
+const { router: postsRoutes, wss } = require("./routes/routes"); // Importing router and WebSocket instance from custom routes module
 
+// Define the port number to listen on, default to 5000 if not provided by the environment
 const PORT = process.env.PORT || 5000;
+
+// Initialize Express application
 const app = express();
 
 // Create HTTP server
 const server = http.createServer(app);
 
-app.use(express.json());
-app.use(cors());
+// Middleware setup
+app.use(express.json()); // Parse incoming JSON requests
+app.use(cors()); // Enable Cross-Origin Resource Sharing for all routes
 
-// Serve static files
+// Serve static files from the 'assets' directory
 app.use("/assets", express.static("assets"));
 
-// Use routes
+// Use the defined routes for handling requests starting with '/posts'
 app.use("/posts", postsRoutes);
 
-// Ensure this part is correctly set up in your server code
+// WebSocket upgrade handling
 server.on("upgrade", (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
     wss.emit("connection", ws, request);
   });
 });
 
-// Listen on the specified port
+// Start the server, listening on the specified port
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
